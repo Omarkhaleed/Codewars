74810a924ea5e79343f10b68c9ee11469d64abb7
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2021-08-03 21:13:57 +0200

    Create 57-Determine offspring sex based on genes XX and XY chromosomes.cpp

---
+++ 57-Determine offspring sex based on genes XX and XY chromosomes.cpp
@@ -0,0 +1,6 @@
+std::string chromosomeCheck(std::string sperm) {
+if(sperm=="XX")
+return "Congratulations! You're going to have a daughter.";
+else
+return "Congratulations! You're going to have a son.";
+}
